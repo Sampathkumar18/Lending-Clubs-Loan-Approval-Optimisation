a47b33b731b2383621ee3873906a32806abadbd8
Sampathkumar Sundaram <68897038+Sampathkumar18@users.noreply.github.com>
2021-04-10 14:53:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('RandomForestClassifier.pkl', 'rb'))
 
 @app.route('/')
 def home():
